8ad0ec292f9afa68b0794241fe604f95743f04f0
melasammmmo-eng <melasammmmo@gmail.com>
2026-08-13 22:57:59 +0300

    Remove toggle command from bot
    Removed the toggle command from the bot.

--- bot.py
+++ bot.py
@@ -316,8 +316,6 @@ async def clearbotserver(interaction: discord.Interaction):
     await interaction.followup.send(f"✅ Finished. Deleted **{total_deleted}** of my messages across the server.", ephemeral=True)
 
 
-@bot.tree.command(name="toggle", description="turns it on and off"
-
 # ================= MESSAGE HANDLER =================
 
 @bot.event
